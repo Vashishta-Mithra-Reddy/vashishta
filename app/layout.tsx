d6a8b0fecefba3bbbce64fa91ba6e8013044c16e
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2025-09-02 14:02:25 +0530

    gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import './globals.css'
 import { Poppins } from 'next/font/google' 
 import localfont from "next/font/local";
+import Script from "next/script";
 
 // import Dock from "@/components/Dock";
 
@@ -30,7 +31,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {
+            `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N5P2W2XP');`
+          }
+        </Script>
+      </head>
       <body className={`${poppins.className} ${aileron.variable}`}>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N5P2W2XP"
+          height="0"
+          width="0"
+          style={{display:"none", visibility:"hidden"}}
+          ></iframe>
+        </noscript>
         {children}
       </body>
     </html>
